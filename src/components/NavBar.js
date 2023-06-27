22e1fa6a68b8628d3ebebf1a7dfb869c59791bf5
HC-kang <weston0713@gmail.com>
2023-06-28 08:40:01 +0900

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,15 +5,40 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link className={router.pathname === '/' ? 'active' : ''} href='/'>
-        home
-      </Link>
-      <Link
-        className={router.pathname === '/about' ? 'active' : ''}
-        href='/about'
-      >
-        about
-      </Link>
+      <img src='/vercel.svg' />
+      <div>
+        <Link href='/' className={router.pathname === '/' ? 'active' : ''}>Home
+        </Link>
+        <Link href='/about' className={router.pathname === '/about' ? 'active' : ''}>About
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,10 @@
-import Layout from '@/components/Layout';
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
-      <style global jsx>{`
-        nav {
-          background-color: tomato;
-        }
-        a {
-          text-decoration: none;
-        }
-        .active {
-          color: yellow;
-        }
-      `}</style>
     </Layout>
   );
 }
